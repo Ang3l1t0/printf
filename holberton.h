aab4cd59b37b4883033bab5a6ba6f54f0721a72f
Ang3l1t0 <1546@holbertonschool.com>
2020-03-16 23:19:07 -0500

    add libraries, add prototypes, add typedef

--- holberton.h
+++ holberton.h
@@ -1,8 +1,24 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
+
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct print_format - structure for print
+ * @t: type to print
+ * @f: function to print
+ */
+
+typedef struct print_format
+{
+	char *t;
+	int (*f)();
+} pt;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-
+int print_char(va_list arg);
+int print_string(va_list arg);
 #endif
